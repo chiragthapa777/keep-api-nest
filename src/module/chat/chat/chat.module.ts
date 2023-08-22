7351dda958bf519238fd54741d146932f85de232
chiragthapa777 <chiragthapa777@gmail.com>
2023-08-22 21:55:18 +0545

    user register fixes

--- src/module/chat/chat/chat.module.ts
+++ src/module/chat/chat/chat.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
+import { ChatWsModule } from "../chat_ws/chat_ws.module";
 
 @Module({
   controllers: [ChatController],
-  providers: [ChatService]
+  providers: [ChatService],
+  imports:[ ChatWsModule]
 })
 export class ChatModule {}
--- src/module/chat/chat/chat.service.ts
+++ src/module/chat/chat/chat.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateChatDto } from './dto/create-chat.dto';
 import { UpdateChatDto } from './dto/update-chat.dto';
+import { ChatWsGateway } from "../chat_ws/chat_ws.gateway";
+import { Server } from 'socket.io';
 
 @Injectable()
 export class ChatService {
+  socket: Server;
+  constructor(private socketws: ChatWsGateway) {
+    this.socket = socketws.server;
+  }
   create(createChatDto: CreateChatDto) {
+    this.socket.emit("notify", {name:"chirag"})
     return 'This action adds a new chat';
   }
 
